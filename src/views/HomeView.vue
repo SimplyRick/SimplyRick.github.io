8999fffa894b84fa435513acd7fda616a6f5fb39
Rick <services.patrick.tomczak@outlook.com>
2025-12-29 00:38:39 +0100

    Fixed router issue and added small changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,6 @@
 	<div class="home">
 		<h1>Welcome to My Portfolio</h1>
 		<p>This is a simple portfolio website built with Vue.js and Vite.js.</p>
-		<h1><a href="https://www.google.de">Google</a></h1>
 	</div>
 </template>
 
